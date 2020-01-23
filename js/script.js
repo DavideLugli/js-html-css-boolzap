21897d3fd4443ac99f90cfda2e4a831eea19488c
Davide Lugli <luglidavide.web@gmail.com>
2020-01-23 19:15:58 +0100

    qualche fix style

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 $(document).ready(
   function() {
     $('#send').keypress(
-      function (event) {
-        if (event.which == 13 || event.keyCode == 13 ) {
-        var text = $('#send').val();
-        $('#send').val('');
-        var newText = $('.template .text-message-wrapper').clone();
-        newText.find('p').append(text);
-        $('.current-chat-screen').append(newText);
+      function(event) {
+        if (event.which == 13 || event.keyCode == 13) {
+          var text = $('#send').val();
+          $('#send').val('');
+          var newText = $('.template .text-message-wrapper').clone();
+          newText.find('p').append(text);
+          $('.current-chat-screen').append(newText);
 
         }
       }
     );
+  }
+);
+$('.fa-telegram-plane').click(
+  function() {
+    var text = $('#send').val();
+    $('#send').val('');
+    var newText = $('.template .text-message-wrapper').clone();
+    newText.find('p').append(text);
+    $('.current-chat-screen').append(newText);
   }
 );
