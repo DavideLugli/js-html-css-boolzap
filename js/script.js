dc09e4735f96c9fcc56fd7216b439df913b9621b
Davide Lugli <luglidavide.web@gmail.com>
2020-01-23 17:43:47 +0100

    invio messaggio funzionante

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+$(document).ready(
+  function() {
+    $('#send').keypress(
+      function (event) {
+        if (event.which == 13 || event.keyCode == 13 ) {
+        var text = $('#send').val();
+        $('#send').val('');
+        var newText = $('.template .text-message-wrapper').clone();
+        newText.find('p').append(text);
+        $('.current-chat-screen').append(newText);
 
+        }
+      }
+    );
+  }
+);
