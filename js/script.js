0d160b40bbc7c31211db8c63107532f404812faf
Davide Lugli <luglidavide.web@gmail.com>
2020-01-24 18:35:33 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,25 +1,40 @@
-$(document).ready(
-  function() {
-    $('#send').keypress(
-      function(event) {
-        if (event.which == 13 || event.keyCode == 13) {
-          var text = $('#send').val();
-          $('#send').val('');
-          var newText = $('.template .text-message-wrapper').clone();
-          newText.find('p').append(text);
-          $('.current-chat-screen').append(newText);
-
-        }
+$(document).ready(function() {
+  $('.icon-send').click(function() {
+    sendMessage();
+  });
+  $('#send').keypress(
+    function(enter) {
+      if (enter.which == 13 || enter.keyCode == 13) {
+        sendMessage();
       }
-    );
-  }
-);
-$('.fa-telegram-plane').click(
-  function() {
-    var text = $('#send').val();
+    });
+});
+
+function sendMessage() {
+  var textMessage = $('#send').val();
+
+  if (textMessage.length != 0) {
+    var newMessage = $('.template .message').clone();
+    console.log(newMessage);
+
+    newMessage.find('.message-text').text(textMessage);
+
+    var data = new Date();
+    var hours = addZero(data.getHours());
+    var minutes = addZero(data.getMinutes());
+    var time = hours + ':' + minutes;
+
+    newMessage.find('.message-time').text(time);
+    newMessage.addClass('sent');
+    $('.current-chat-screen').append(newMessage);
+
     $('#send').val('');
-    var newText = $('.template .text-message-wrapper').clone();
-    newText.find('p').append(text);
-    $('.current-chat-screen').append(newText);
   }
-);
+}
+
+function addZero(number) {
+  if (number < 10) {
+    number = '0' + number;
+  }
+  return number;
+}
