8433ec18aefd9d4f982dc9481b60195fb1441f98
Davide Lugli <luglidavide.web@gmail.com>
2020-01-25 16:01:28 +0100

    risposta auto

--- js/script.js
+++ js/script.js
@@ -15,23 +15,34 @@ function sendMessage() {
 
   if (textMessage.length != 0) {
     var newMessage = $('.template .message').clone();
-    console.log(newMessage);
-
     newMessage.find('.message-text').text(textMessage);
-
+    // aggiungo ora corrente al messaggio
     var data = new Date();
     var hours = addZero(data.getHours());
     var minutes = addZero(data.getMinutes());
     var time = hours + ':' + minutes;
-
     newMessage.find('.message-time').text(time);
+    // aggiungo classe sent e testo del messaggio
     newMessage.addClass('sent');
     $('.current-chat-screen').append(newMessage);
-
+    // svuoto l'input
     $('#send').val('');
+    // funzione di risposta automatica dopo 1 sec
+    setTimeout(function() {
+      var autoReply = $('.template .message').clone();
+      autoReply.find('.message-text').text('Fantastico!');
+      var data = new Date();
+      var hours = addZero(data.getHours());
+      var minutes = addZero(data.getMinutes());
+      var time = hours + ':' + minutes;
+      autoReply.find('.message-time').text(time);
+      autoReply.addClass('received');
+      $('.current-chat-screen').append(autoReply);
+    }, 1000);
   }
 }
 
+// aggiunge uno 0 ai minuti nell'ora nel caso i minuti attuali siano <10
 function addZero(number) {
   if (number < 10) {
     number = '0' + number;
